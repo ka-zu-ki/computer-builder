a44fbab2ebb8c05e605f926852e0af1507693d2d
ka-zu-ki <yamakazuki0721@gmail.com>
2021-09-09 18:50:19 +0900

    create config

---
+++ src/ts/View.ts
@@ -0,0 +1,21 @@
+import {
+  cpuModel,
+  cpuBrand,
+  gpuBrand,
+  gpuModel,
+  ramAmount,
+  ramBrand,
+  ramModel,
+  storageKind,
+  storage,
+  storageBrand,
+  storageModel,
+  btn,
+  score,
+} from './config';
+
+class Vies {
+  static selectCpu() {
+    
+  }
+}
---
+++ src/ts/config.ts
@@ -0,0 +1,29 @@
+const cpuBrand = document.getElementById('cpuBrand');
+const cpuModel = document.getElementById('cpuModel');
+const gpuBrand = document.getElementById('gpuBrand');
+const gpuModel = document.getElementById('gpuModel');
+const ramAmount = document.getElementById('ramAmount');
+const ramBrand = document.getElementById('ramBrand');
+const ramModel = document.getElementById('ramModel');
+const storageKind = document.getElementById('storageKind');
+const storage = document.getElementById('storage');
+const storageBrand = document.getElementById('storageBrand');
+const storageModel = document.getElementById('storageModel');
+const btn = document.getElementById('btn');
+const score = document.getElementById('score');
+
+export {
+  cpuModel,
+  cpuBrand,
+  gpuBrand,
+  gpuModel,
+  ramAmount,
+  ramBrand,
+  ramModel,
+  storageKind,
+  storage,
+  storageBrand,
+  storageModel,
+  btn,
+  score,
+};
